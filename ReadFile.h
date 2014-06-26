d553435e9d70e67a5030c7f15b9e1d00a431f2dc
Mark Boshart <mboshart@tntech.edu>
2014-06-26 15:10:12 -0500

    changes made to ReadFile.h

--- ReadFile.h
+++ ReadFile.h
@@ -6,17 +6,21 @@
 #include <fstream>
 using namespace std;
 
-struct ReadFile
+class ReadFile
 {
-   ifstream input_file;
-   bool _eof;
-   bool closed;
+   private:
+      ifstream input_file;
+      bool _eof;
+      bool closed;
+
+   public:
+      ReadFile();
+      virtual ~ReadFile();
+      String* readLine();
+      bool eof();
+      void close();
 };
 
-ReadFile* createReadFile(const char* file_name);
-void destroyReadFile(ReadFile* rf);
-String* readLine(ReadFile* rf);
-bool eof(ReadFile* rf);
-void close(ReadFile* rf);
+
 
 #endif
